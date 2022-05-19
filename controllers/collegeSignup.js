4d07267a4f1478677e564e38d6fdc8aaace09e84
porwalarchit <architporwal@gmail.com>
2022-05-19 12:28:09 +0530

    College Signup Api Done

--- controllers/collegeSignup.js
+++ controllers/collegeSignup.js
@@ -4,17 +4,15 @@ const College = require("../models/collegeModel");
 module.exports =  collegeSignup = async (req, res) => {
     try {
         if (req.body.password == req.body.confirmpassword) {
-            const new_User = new College({
+            const newUser = new College({
                 collegeName: req.body.collegeName,
                 email: req.body.email,
             })
 
             const password = req.body.password;
 
-            // generate salt to hash password
-            // const salt = await bcrypt.genSalt(10);
-            // now we set user password to hashed password
-            new_User.hashPassword = await bcrypt.hash(password, 10);
+            // now we set newUser password to hashed password
+            newUser.hashPassword = await bcrypt.hash(password, 10);
 
             await new_User.save();
             return res.status(200).json({
