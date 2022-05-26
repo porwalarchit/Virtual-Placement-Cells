790ba429e39669cec9e8a174cab8933ba5b64824
porwalarchit <architporwal@gmail.com>
2022-05-26 19:25:56 +0530

    Get All Company Done

--- routes/collegeRoutes.js
+++ routes/collegeRoutes.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const authenticateToken = require("../middlewares/authenticateToken");
-const upload = require("../utils/multer");
 
+const authenticateToken = require("../middlewares/authenticateToken");
+
+const collegeSignup = require("../controllers/collegeSignup");
+const collegeLogin = require("../controllers/collegeLogin");
+const getCollegeProfile = require("../controllers/getCollegeProfile");
+const updateCollegeProfile = require("../controllers/updateCollegeProfile");
 const addPlacement = require("../controllers/addPlacement");
 const deletePlacement = require("../controllers/deletePlacement");
+const getAllCompany = require("../controllers/getAllCompany");
+const upload = require("../utils/multer");
+const Company = require("../models/companyModel");
+
+
+router.post('/register', collegeSignup);
+
+router.post('/login', collegeLogin);
+
+router.get('/profile', authenticateToken, getCollegeProfile);
+
+router.patch('/update/profile', authenticateToken, upload.single("profileImg"), updateCollegeProfile);
 
 router.post('/addPlacement', authenticateToken, upload.single("studentImage"), addPlacement);
 
 router.delete('/deletePlacement/:id', authenticateToken, deletePlacement);
 
-router.get('/getAllCompany', authenticateToken, async(req, res)=>{
-    try {
-        
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-})
+router.get('/getAllCompany', authenticateToken, getAllCompany);
 
 module.exports = router;
--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -2,15 +2,29 @@ const express = require("express");
 const router = express.Router();
 
 const authenticateToken = require("../middlewares/authenticateToken");
+
+const companySignup = require("../controllers/companySignup");
+const companyLogin = require("../controllers/companyLogin");
+const getCompanyProfile = require("../controllers/getCompanyProfile");
+const updateCompanyProfile = require("../controllers/updateCompanyProfile");
 const addJob = require("../controllers/addJob");
 const getAllCollege = require("../controllers/getAllCollege");
 const getAllCollegePlacements = require("../controllers/getAllCollegePlacements");
+const upload = require("../utils/multer");
 
+
+router.post('/signup', companySignup);
+
+router.post('/login', companyLogin)
+
+router.get('/profile', authenticateToken, getCompanyProfile);
+
+router.patch('/update/profile', authenticateToken, upload.single("profileImg"),updateCompanyProfile);
 
 router.post('/addJob', authenticateToken, addJob);
 
 router.get('/getAllCollege', authenticateToken, getAllCollege);
 
-router.get('/getAllCollege/:id/placements', authenticateToken, getAllCollegePlacements);
+router.get('/getAllCollege/placements/:id', authenticateToken, getAllCollegePlacements);
 
 module.exports = router;
